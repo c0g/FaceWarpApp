8538ccd6ed662cf3f0f6914011c79f817175f87b
Thomas Gunter <tbgunter@Thomass-MacBook-Pro.local>
2015-09-24 00:28:24 +0100

    added missing frameworks

---
+++ FaceWarpApp/dlib/build/dlib_build/CMakeFiles/dlib.dir/DependInfo.cmake
@@ -0,0 +1,114 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/base64/base64_kernel_1.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/base64/base64_kernel_1.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/bigint/bigint_kernel_1.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/bigint/bigint_kernel_1.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/bigint/bigint_kernel_2.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/bigint/bigint_kernel_2.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/bit_stream/bit_stream_kernel_1.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/bit_stream/bit_stream_kernel_1.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/bsp/bsp.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/bsp/bsp.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/data_io/image_dataset_metadata.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/data_io/image_dataset_metadata.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/dir_nav/dir_nav_extensions.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/dir_nav/dir_nav_extensions.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/dir_nav/dir_nav_kernel_1.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/dir_nav/dir_nav_kernel_1.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/dir_nav/dir_nav_kernel_2.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/dir_nav/dir_nav_kernel_2.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/entropy_decoder/entropy_decoder_kernel_1.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/entropy_decoder/entropy_decoder_kernel_1.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/entropy_decoder/entropy_decoder_kernel_2.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/entropy_decoder/entropy_decoder_kernel_2.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/entropy_encoder/entropy_encoder_kernel_1.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/entropy_encoder/entropy_encoder_kernel_1.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/entropy_encoder/entropy_encoder_kernel_2.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/entropy_encoder/entropy_encoder_kernel_2.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jcapimin.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jcapimin.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jcapistd.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jcapistd.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jccoefct.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jccoefct.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jccolor.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jccolor.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jcdctmgr.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jcdctmgr.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jchuff.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jchuff.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jcinit.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jcinit.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jcmainct.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jcmainct.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jcmarker.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jcmarker.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jcmaster.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jcmaster.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jcomapi.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jcomapi.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jcparam.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jcparam.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jcphuff.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jcphuff.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jcprepct.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jcprepct.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jcsample.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jcsample.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jdapimin.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jdapimin.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jdapistd.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jdapistd.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jdatadst.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jdatadst.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jdatasrc.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jdatasrc.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jdcoefct.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jdcoefct.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jdcolor.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jdcolor.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jddctmgr.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jddctmgr.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jdhuff.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jdhuff.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jdinput.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jdinput.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jdmainct.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jdmainct.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jdmarker.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jdmarker.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jdmaster.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jdmaster.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jdmerge.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jdmerge.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jdphuff.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jdphuff.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jdpostct.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jdpostct.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jdsample.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jdsample.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jerror.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jerror.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jfdctflt.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jfdctflt.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jfdctfst.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jfdctfst.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jfdctint.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jfdctint.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jidctflt.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jidctflt.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jidctfst.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jidctfst.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jidctint.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jidctint.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jidctred.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jidctred.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jmemmgr.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jmemmgr.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jmemnobs.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jmemnobs.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jquant1.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jquant1.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jquant2.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jquant2.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/external/libjpeg/jutils.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/external/libjpeg/jutils.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/image_loader/jpeg_loader.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/image_loader/jpeg_loader.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/image_loader/png_loader.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/image_loader/png_loader.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/image_saver/save_jpeg.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/image_saver/save_jpeg.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/image_saver/save_png.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/image_saver/save_png.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/linker/linker_kernel_1.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/linker/linker_kernel_1.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/logger/extra_logger_headers.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/logger/extra_logger_headers.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/logger/logger_config_file.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/logger/logger_config_file.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/logger/logger_kernel_1.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/logger/logger_kernel_1.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/md5/md5_kernel_1.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/md5/md5_kernel_1.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/misc_api/misc_api_kernel_1.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/misc_api/misc_api_kernel_1.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/misc_api/misc_api_kernel_2.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/misc_api/misc_api_kernel_2.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/server/server_http.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/server/server_http.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/server/server_iostream.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/server/server_iostream.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/server/server_kernel.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/server/server_kernel.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/sockets/sockets_extensions.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/sockets/sockets_extensions.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/sockets/sockets_kernel_1.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/sockets/sockets_kernel_1.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/sockets/sockets_kernel_2.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/sockets/sockets_kernel_2.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/sockstreambuf/sockstreambuf.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/sockstreambuf/sockstreambuf.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/sockstreambuf/sockstreambuf_unbuffered.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/sockstreambuf/sockstreambuf_unbuffered.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/stack_trace.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/stack_trace.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/threads/multithreaded_object_extension.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/threads/multithreaded_object_extension.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/threads/thread_pool_extension.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/threads/thread_pool_extension.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/threads/threaded_object_extension.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/threads/threaded_object_extension.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/threads/threads_kernel_1.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/threads/threads_kernel_1.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/threads/threads_kernel_2.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/threads/threads_kernel_2.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/threads/threads_kernel_shared.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/threads/threads_kernel_shared.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/timer/timer.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/timer/timer.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/tokenizer/tokenizer_kernel_1.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/tokenizer/tokenizer_kernel_1.o"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/unicode/unicode.cpp" "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/build/dlib_build/CMakeFiles/dlib.dir/unicode/unicode.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "DLIB_JPEG_STATIC"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Users/tbgunter/anaconda/envs/_build/include"
+  "/Users/tbgunter/anaconda/envs/_build/include/python2.7"
+  "/Users/tbgunter/anaconda/conda-bld/work/dlib-18.16/dlib/.."
+  "/Users/tbgunter/anaconda/envs/_build"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
